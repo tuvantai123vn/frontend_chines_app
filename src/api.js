9d9e99d400999dcbcfe39c5f00ba3130bef72efd
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2025-04-25 23:21:39 +0700

    update2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api", // Đặt baseURL chung
+    baseURL: "https://backend-chines-learning.onrender.com/api", // Đặt baseURL chung
     headers: {
       "Content-Type": "application/json",
     },
