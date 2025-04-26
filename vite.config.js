aa11d76ac200f20f54ff10162999c49fdb719532
Nguyễn_Thanh_Tú <75940246+tuvantai123vn@users.noreply.github.com>
2025-04-26 03:51:04 +0000

    update_2024_04_26_1

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-  ]
-  
+  ],
+  server: {
+    historyApiFallback: true, // Giúp React Router xử lý route đúng khi reload hoặc nhập đường dẫn
+  }
 })
